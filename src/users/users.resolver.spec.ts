4c526177a8f854b88863e74636c3ebdd3d0138f6
Andre <itstheandre@gmail.com>
2021-01-10 10:06:01 +0100

    chore() setup for pair programming

---
+++ src/users/users.resolver.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersResolver } from './users.resolver';
+
+describe('UsersResolver', () => {
+  let resolver: UsersResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersResolver],
+    }).compile();
+
+    resolver = module.get<UsersResolver>(UsersResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  it('sends back hello', () => {
+    expect(resolver.hello()).toMatch(/hello/gi);
+  });
+});
